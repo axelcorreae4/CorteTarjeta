63e934bd52e45492d8d88c7556282b33c7c7caa1
axelcorreae <axellink4@gmail.com>
2024-08-10 19:39:55 -0600

    Creacion
    Creacion del proyecto

---
+++ Corte-Tarjeta-Credito/src/main/java/com/axel/cortes/bancarios/Corte/Tarjeta/Credito/CorteTarjetaCreditoApplication.java
@@ -0,0 +1,13 @@
+package com.axel.cortes.bancarios.Corte.Tarjeta.Credito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorteTarjetaCreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorteTarjetaCreditoApplication.class, args);
+	}
+
+}
---
+++ Corte-Tarjeta-Credito/src/main/java/com/axel/cortes/bancarios/Corte/Tarjeta/Credito/model/entity/Corte.java
@@ -0,0 +1,4 @@
+package com.axel.cortes.bancarios.Corte.Tarjeta.Credito.model.entity;
+
+public class Corte {
+}
